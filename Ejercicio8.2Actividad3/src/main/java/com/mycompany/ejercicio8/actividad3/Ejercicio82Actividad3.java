27b28f729a8ad4b0d0487b58171d0793863939b8
caroooes <caespinosaj@unal.edu.co>
2025-10-14 20:53:01 -0500

    ejercicio

---
+++ Ejercicio8.2Actividad3/src/main/java/com/mycompany/ejercicio8/actividad3/Ejercicio82Actividad3.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio8.actividad3;
+
+/**
+ *
+ * @author Caro
+ */
+public class Ejercicio82Actividad3 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal formulario = new VentanaPrincipal ();
+        formulario.setVisible(true);
+    }
+}
